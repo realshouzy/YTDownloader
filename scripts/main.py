7f5ca8d28ea651c9949cf548a8744bc0803feaba
shouzy <shouzy@tuta.io>
2022-06-25 20:12:38 +0200

    renamed YTDownloader.py to main.py + updated exception handling

--- scripts/main.py
+++ scripts/main.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 # -*- coding: UTF-8 -*-
 import PySimpleGUI as sg
-from pytube.exceptions import RegexMatchError
+from pytube.exceptions import RegexMatchError, VideoUnavailable
 from windows import VideoDownloadWindow
 
 
@@ -31,6 +31,9 @@ def main() -> None:
                 sg.Popup(f'Error: {type(rmx).__name__}', custom_text='Please provide link', title='Error')
             else:
                 sg.Popup(f'Error: {type(rmx).__name__}', custom_text='Invalid link', title='Error')
+
+        except VideoUnavailable as vux:
+            sg.Popup(f'Error: {type(vux).__name__}', custom_text='Video Unavailable', title='Error')
 
         except Exception as x:
             sg.Popup(f'{type(x).__name__} at line {x.__traceback__.tb_lineno} of {__file__}: {x}', custom_text='Unexpected error', title='Error') 

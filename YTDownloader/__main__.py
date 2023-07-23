c0f919e2a56d48dd55e5a70df1368e942c17da31
shouzy <82171453+realshouzy@users.noreply.github.com>
2023-07-23 23:54:11 +0200

    Added __main__.py

---
+++ YTDownloader/__main__.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+"""Run module as file."""
+from __future__ import annotations
+
+from YTDownloader.main import main
+
+if __name__ == "__main__":
+    raise SystemExit(main())

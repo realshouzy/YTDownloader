402bfc47827fa50703e200c3b7c6cf998adadf1c
shouzy <82171453+realshouzy@users.noreply.github.com>
2023-07-28 19:42:12 +0200

    Kinda refactored try-except

--- YTDownloader/main.py
+++ YTDownloader/main.py
@@ -27,63 +27,66 @@ def main() -> int:  # noqa: C901
 
     # main event loop
     while True:
-        try:
-            event, values = start_window.read()  # type: ignore
-            if event == sg.WIN_CLOSED:
-                break
+        event, values = start_window.read()
+        if event == sg.WIN_CLOSED:
+            break
 
-            if event == "Submit":
+        if event == "Submit":
+            try:
                 youtube_downloader: YouTubeDownloader = YouTubeDownloader(
                     values["-LINKINPUT-"],
                 )
                 youtube_downloader.create_window()
 
-        except pytube.exceptions.RegexMatchError as re_err:
-            if not values["-LINKINPUT-"]:  # type: ignore
+            except pytube.exceptions.RegexMatchError as re_err:
+                if not values["-LINKINPUT-"]:
+                    create_error_window(
+                        re_err.__class__.__name__,
+                        "Please provide link.",
+                    )
+                else:
+                    create_error_window(re_err.__class__.__name__, "Invalid link.")
+
+            except pytube.exceptions.VideoPrivate as vp_err:
+                create_error_window(vp_err.__class__.__name__, "Video is privat.")
+
+            except pytube.exceptions.MembersOnly as mo_err:
                 create_error_window(
-                    re_err.__class__.__name__,
-                    "Please provide link.",
+                    mo_err.__class__.__name__,
+                    "Video is for members only.",
                 )
-            else:
-                create_error_window(re_err.__class__.__name__, "Invalid link.")
 
-        except pytube.exceptions.VideoPrivate as vp_err:
-            create_error_window(vp_err.__class__.__name__, "Video is privat.")
+            except pytube.exceptions.VideoRegionBlocked as vgb_err:
+                create_error_window(
+                    vgb_err.__class__.__name__,
+                    "Video is block in your region.",
+                )
 
-        except pytube.exceptions.MembersOnly as mo_err:
-            create_error_window(mo_err.__class__.__name__, "Video is for members only.")
+            except pytube.exceptions.LiveStreamError as ls_err:
+                create_error_window(
+                    ls_err.__class__.__name__,
+                    "This is an active life stream.",
+                )
 
-        except pytube.exceptions.VideoRegionBlocked as vgb_err:
-            create_error_window(
-                vgb_err.__class__.__name__,
-                "Video is block in your region.",
-            )
+            except pytube.exceptions.AgeRestrictedError as ar_err:
+                create_error_window(
+                    ar_err.__class__.__name__,
+                    "This video is age restricted.",
+                )
 
-        except pytube.exceptions.LiveStreamError as ls_err:
-            create_error_window(
-                ls_err.__class__.__name__,
-                "This is an active life stream.",
-            )
+            except pytube.exceptions.VideoUnavailable as vu_err:
+                create_error_window(vu_err.__class__.__name__, "Video Unavailable.")
 
-        except pytube.exceptions.AgeRestrictedError as ar_err:
-            create_error_window(
-                ar_err.__class__.__name__,
-                "This video is age restricted.",
-            )
+            except KeyError as key_err:
+                create_error_window(
+                    key_err.__class__.__name__,
+                    "Video or playlist is unreachable or invalid.",
+                )
 
-        except pytube.exceptions.VideoUnavailable as vu_err:
-            create_error_window(vu_err.__class__.__name__, "Video Unavailable.")
-
-        except KeyError as key_err:
-            create_error_window(
-                key_err.__class__.__name__,
-                "Video or playlist is unreachable or invalid.",
-            )
-
-        except Exception as err:
-            create_error_window(err.__class__.__name__, str(err))
-            exit_code = 1
-            break
+            except Exception as err:
+                create_error_window(err.__class__.__name__, str(err))
+                exit_code = 1
+                break
 
     start_window.close()
     return exit_code

cfe0f0a71ff41597eb89e4b4a59ce07e7921c1b7
shouzy <82171453+realshouzy@users.noreply.github.com>
2024-06-30 22:27:43 +0200

    Bump version to 1.5.5

--- YTDownloader.py
+++ YTDownloader.py
@@ -2,7 +2,7 @@
 
 from __future__ import annotations
 
-__version__: Final[str] = "1.5.4"
+__version__: Final[str] = "1.5.5"
 __title__: Final[str] = "YTDownloader"
 __author__: Final[str] = "realshouzy"
 __license__: Final[str] = "MIT"
